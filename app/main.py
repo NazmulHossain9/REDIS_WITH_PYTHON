55c71f7bf6ddacd51db385ac5f6ac39ba49a7d1a
NazmulHossain9 <mn.cse48@gmail.com>
2026-04-22 12:50:09 +0600

    connection

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
+import socket
+
 def main():
     print("Hello from redis-with-python!")
+    server_socket= socket.create_server(('localhost', 6379), reuse_port=True)
+    server_socket.accept()  # This will block until a client connects
+    # print("Client connected!") # wait for client
 
 
 if __name__ == "__main__":
