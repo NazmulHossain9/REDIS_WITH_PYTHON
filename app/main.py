1c52c322b458ec45fe2e68e8ded71226080e2a59
NazmulHossain9 <mn.cse48@gmail.com>
2026-04-22 13:17:00 +0600

    Redis Server Always run

--- app/main.py
+++ app/main.py
@@ -4,11 +4,15 @@ def main():
     print("Hello from redis-with-python!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    # server_socket.accept() # wait for client
-    connection, _ = server_socket.accept()
-    connection.sendall(b"+PONG\r\n")
 
-
+    while True:
+        connection, _ = server_socket.accept()
+        while True:
+            data = connection.recv(1024)
+            if not data:
+                break
+            if b"PING" in data.upper():
+                connection.sendall(b"+PONG\r\n")
 
 if __name__ == "__main__":
     main()
